f5f7d18741cd045e961680b9de8324e71e7cddc0
Adory Vo <business@adoryvo.com>
2026-09-12 00:43:41 -0700

    feat: revert unimpactful battle difficulty commit

--- src/new_game.c
+++ src/new_game.c
@@ -236,7 +236,7 @@ void NewGameInitData(void)
     ResetTrainerHillResults();
     ResetTrainerTowerResults();
     ResetContestLinkResults();
-    SetCurrentDifficultyLevel(DIFFICULTY_HARD);
+    SetCurrentDifficultyLevel(DIFFICULTY_NORMAL);
     ResetItemFlags();
     ResetDexNav();
     ClearFollowerNPCData();
